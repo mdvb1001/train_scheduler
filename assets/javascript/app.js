e5e8ae9334e4a285c170a59131b7d83fca148909
maxvanbel <Max@Maximes-MacBook-Pro.local>
2016-11-15 22:58:27 -0600

    add firebase to app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,8 +13,15 @@
 
 	PHASE 2 - Get Firebase working on basic level
 
-
-
-
 */
 
+var config = {
+    apiKey: "AIzaSyA7GxLKi8V3D-CBZJhTw2NazOWYoGY41l8",
+    authDomain: "train-scheduler-b7e2c.firebaseapp.com",
+    databaseURL: "https://train-scheduler-b7e2c.firebaseio.com",
+    storageBucket: "train-scheduler-b7e2c.appspot.com",
+    messagingSenderId: "1016520910023"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
