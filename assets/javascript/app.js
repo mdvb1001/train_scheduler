285403c185040736a2e5b0ae96e92c79f3ef7a42
maxvanbel <Max@Maximes-MacBook-Pro.local>
2016-11-15 21:53:46 -0600

    pseudo coding part 1

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+
+
+
+// PHASE 1 - Get the UI working	
+// 	Jumbotron for the header
+// 	For upcoming train: Form with panel and legend
+//  For train input: Form with panel, legend:
+/*   * train name 
+	 * Destination
+	 * First train time - in military time
+	 * Frequency - in minutes 
+
+
+*/
