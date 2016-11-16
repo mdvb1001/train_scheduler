17715789df518df98901b834c5d05635ce704929
maxvanbel <Max@Maximes-MacBook-Pro.local>
2016-11-15 23:35:08 -0600

    add firebase data to html page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,3 +25,48 @@ var config = {
   firebase.initializeApp(config);
 
   var database = firebase.database();
+
+  var name = "";
+  var destination = "";
+  var firstTrainTime = "";
+  var frequency = 0;
+
+  $(document).on('ready', function() {
+  	$('#submit').on('click', function(){
+  		name = $('#inputName').val().trim();
+  		destination = $('#inputDestination').val().trim();
+  		firstTrainTime = $('#inputFirstTime').val().trim();
+  		frequency = $('#inputFrequency').val().trim();
+
+  	database.ref().push({
+  		name: name,
+  		destination: destination,
+  		firstTrainTime: firstTrainTime,
+  		frequency: frequency
+  	});
+
+  	$('#inputName').val('');
+    $('#inputDestination').val('');
+    $('#inputFirstTime').val('');
+    $('#inputFrequency').val('');
+
+    return false;
+  	});
+
+  	database.ref().on("child_added", function(childSnapshot){
+
+  		var tableRow = $('<tr>');
+
+  		var nameCell = $('<td>').text(childSnapshot.val().name);
+  		var destinationCell = $('<td>').text(childSnapshot.val().destination);
+  		var firstTrainTimeCell = $('<td>').text(childSnapshot.val().firstTrainTime);
+  		var frequencyCell = $('<td>').text(childSnapshot.val().frequency);
+
+  		tableRow.append(nameCell).append(destinationCell).append(frequencyCell).append(firstTrainTimeCell);
+
+  		$('tbody').append(tableRow);
+  	});
+
+
+
+  });
