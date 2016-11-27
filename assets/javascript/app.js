0a843f0b86298e34eb7d9ee9e37d603407441fc8
maxvanbel <Max@maximes-mbp.attlocal.net>
2016-11-27 13:18:24 -0600

    add nextTrainTime and MinutesTillTrain in HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,3 @@
-
-
-
 /*  PHASE 1 - Get the UI working
 
  	Jumbotron for the header
@@ -14,74 +11,100 @@
 	PHASE 2 - Get Firebase working on basic level
 
 */
-
 var config = {
     apiKey: "AIzaSyA7GxLKi8V3D-CBZJhTw2NazOWYoGY41l8",
     authDomain: "train-scheduler-b7e2c.firebaseapp.com",
     databaseURL: "https://train-scheduler-b7e2c.firebaseio.com",
     storageBucket: "train-scheduler-b7e2c.appspot.com",
     messagingSenderId: "1016520910023"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+var name = "";
+var destination = "";
 
-  var database = firebase.database();
-
-  var name = "";
-  var destination = "";
-  var firstTrainTime = "";
-  var frequency = 0;
-  
-  
-  var numberOfTrainsSinceFirstTrain = ((startTime - currentTime) % frequency);
-  var nexTrainTime = (frequency * numberOfTrainsSinceFirstTrain) + firstTrainTime;
-
-  var startTime = moment(firstTrainTime, 'hh:mm a');
-  var startTime = moment("12:16 am", 'hh:mm a');
-  var endTime = moment("06:12 pm", 'hh:mm a');
-
-      endTime.diff(startTime, 'minutes');
-
-
-
-
-  $(document).on('ready', function() {
-  	$('#submit').on('click', function(){
-  		name = $('#inputName').val().trim();
-  		destination = $('#inputDestination').val().trim();
-  		firstTrainTime = $('#inputFirstTime').val().trim();
-  		frequency = $('#inputFrequency').val().trim();
-
-  	database.ref().push({
-  		name: name,
-  		destination: destination,
-  		firstTrainTime: firstTrainTime,
-  		frequency: frequency,
-  		dateAdded: firebase.database.ServerValue.TIMESTAMP
-  	});
-
-  	$('#inputName').val('');
-    $('#inputDestination').val('');
-    $('#inputFirstTime').val('');
-    $('#inputFrequency').val('');
-
-    return false;
-  	});
-  	//freq * the 5th train + the initial time = next train
-  	database.ref().on("child_added", function(childSnapshot){
-  		
-  		var tableRow = $('<tr>');
-  		var nameCell = $('<td>').text(childSnapshot.val().name);
-  		var destinationCell = $('<td>').text(childSnapshot.val().destination);
-  		var firstTrainTimeCell = $('<td>').text(childSnapshot.val().firstTrainTime);
-  		var frequencyCell = $('<td>').text(childSnapshot.val().frequency);
-      var nextTrainTimeCell = $('<td>').text(childSnapshot.val().nextTrainTime);
-      // moment().add(1, 'hours').diff(moment(), 'minutes')
-      // moment().diff(moment(childSnapshot.val().firstTrainTime), "minutes");
-  		tableRow.append(nameCell).append(destinationCell).append(frequencyCell).append(nextTrainTimeCell);
-
-  		$('tbody').append(tableRow);
-  	});
+// Assumptions
+var frequency = 0;
+// Time is 3:30 AM
+var firstTrainTime = "";
+// First Time (pushed back 1 year to make sure it comes before current time)
+var firstTimeConverted = "";
+// Current Time
+var currentTime = "";
+// Difference between the times
+var diffTime = "";
+// Time apart (remainder)
+var tRemainder = "";
+// Minute Until Train
+var tMinutesTillTrain = "";
+// Next Train
+var nextTrainTime = "";
 
 
+// var numberOfTrainsSinceFirstTrain = ((startTime - currentTime) % frequency);
+// var nexTrainTime = (frequency * numberOfTrainsSinceFirstTrain) + firstTrainTime;
+// var startTime = moment(firstTrainTime, 'hh:mm a');
+// var startTime = moment("12:16 am", 'hh:mm a');
+// var endTime = moment("06:12 pm", 'hh:mm a');
+//     endTime.diff(startTime, 'minutes');
+$(document).on('ready', function () {
+    $('#submit').on('click', function () {
+        name = $('#inputName').val().trim();
+        destination = $('#inputDestination').val().trim();
+        firstTrainTime = $('#inputFirstTime').val().trim();
+        frequency = $('#inputFrequency').val().trim();
 
-  });
+        database.ref().push({
+            name: name,
+            destination: destination,
+            firstTrainTime: firstTrainTime,
+            frequency: frequency,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+        $('#inputName').val('');
+        $('#inputDestination').val('');
+        $('#inputFirstTime').val('');
+        $('#inputFrequency').val('');
+        return false;
+    });
+    //freq * the 5th train + the initial time = next train
+    database.ref().on("child_added", function (childSnapshot) {
+        
+        // First Time (pushed back 1 year to make sure it comes before current time)
+        var firstTimeConverted = moment(childSnapshot.val().firstTrainTime, "hh:mm").subtract(1, "years");
+        console.log(firstTimeConverted);
+
+        // Current Time
+        var currentTime = moment();
+        console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+        // Difference between the times
+        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        console.log("DIFFERENCE IN TIME: " + diffTime);
+
+        // Time apart (remainder)
+        var tRemainder = diffTime % childSnapshot.val().frequency;
+        console.log("TIME APART: " + tRemainder);
+
+        // Minute Until Train
+        var tMinutesTillTrain = childSnapshot.val().frequency - tRemainder;
+        console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+        // Next Train
+        var nextTrainTime = moment().add(tMinutesTillTrain, "minutes").format("HH:mm");
+        console.log("ARRIVAL TIME: " + moment(nextTrainTime).format("HH:mm"));
+
+        var tableRow = $('<tr>');
+        var nameCell = $('<td>').text(childSnapshot.val().name);
+        var destinationCell = $('<td>').text(childSnapshot.val().destination);
+        var frequencyCell = $('<td>').text(childSnapshot.val().frequency);
+        var firstTrainTimeCell = $('<td>').text(childSnapshot.val().firstTrainTime);
+        var nextTrainTimeCell = $('<td>').text(nextTrainTime);
+        var tMinutesTillTrainCell = $('<td>').text(tMinutesTillTrain);
+        // moment().add(1, 'hours').diff(moment(), 'minutes')
+        // moment().diff(moment(childSnapshot.val().firstTrainTime), "minutes");
+        
+        tableRow.append(nameCell).append(destinationCell).append(frequencyCell).append(firstTrainTimeCell).append(nextTrainTimeCell).append(tMinutesTillTrainCell);
+        $('tbody').append(tableRow);
+    });
+});
