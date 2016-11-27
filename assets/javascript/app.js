316c31b8e75c95196479ca77e420231ca4b85989
maxvanbel <Max@maximes-mbp.attlocal.net>
2016-11-27 00:08:55 -0600

    next train struggles

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -30,6 +30,19 @@ var config = {
   var destination = "";
   var firstTrainTime = "";
   var frequency = 0;
+  
+  
+  var numberOfTrainsSinceFirstTrain = ((startTime - currentTime) % frequency);
+  var nexTrainTime = (frequency * numberOfTrainsSinceFirstTrain) + firstTrainTime;
+
+  var startTime = moment(firstTrainTime, 'hh:mm a');
+  var startTime = moment("12:16 am", 'hh:mm a');
+  var endTime = moment("06:12 pm", 'hh:mm a');
+
+      endTime.diff(startTime, 'minutes');
+
+
+
 
   $(document).on('ready', function() {
   	$('#submit').on('click', function(){
@@ -42,7 +55,8 @@ var config = {
   		name: name,
   		destination: destination,
   		firstTrainTime: firstTrainTime,
-  		frequency: frequency
+  		frequency: frequency,
+  		dateAdded: firebase.database.ServerValue.TIMESTAMP
   	});
 
   	$('#inputName').val('');
@@ -52,17 +66,18 @@ var config = {
 
     return false;
   	});
-
+  	//freq * the 5th train + the initial time = next train
   	database.ref().on("child_added", function(childSnapshot){
-
+  		
   		var tableRow = $('<tr>');
-
   		var nameCell = $('<td>').text(childSnapshot.val().name);
   		var destinationCell = $('<td>').text(childSnapshot.val().destination);
   		var firstTrainTimeCell = $('<td>').text(childSnapshot.val().firstTrainTime);
   		var frequencyCell = $('<td>').text(childSnapshot.val().frequency);
-
-  		tableRow.append(nameCell).append(destinationCell).append(frequencyCell).append(firstTrainTimeCell);
+      var nextTrainTimeCell = $('<td>').text(childSnapshot.val().nextTrainTime);
+      // moment().add(1, 'hours').diff(moment(), 'minutes')
+      // moment().diff(moment(childSnapshot.val().firstTrainTime), "minutes");
+  		tableRow.append(nameCell).append(destinationCell).append(frequencyCell).append(nextTrainTimeCell);
 
   		$('tbody').append(tableRow);
   	});
